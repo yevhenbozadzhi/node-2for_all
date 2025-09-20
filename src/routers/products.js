678ee28ca798ab5927030b0ccf3faee266d77e3f
Sergey-8057 <erste_sergey@proton.me>
2025-09-20 15:24:20 +0300

    task-4

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { getProductsControllers } from '../controllers/products.js';
+import {
+  getProductsControllers,
+  getProductByIdController,
+} from '../controllers/products.js';
 
 const router = Router();
 
 router.get('/products', getProductsControllers);
+router.get('/products/:productId', getProductByIdController);
 
 export default router;
--- src/services/products.js
+++ src/services/products.js
@@ -3,3 +3,7 @@ import { ProductModel } from '../db/models/products.js';
 export const getProducts = () => {
   return ProductModel.find();
 };
+
+export const getProductById = (productId) => {
+  return ProductModel.findById(productId);
+};
