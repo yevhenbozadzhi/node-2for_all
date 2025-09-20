b4a5b8b20103bca2393280e7b85fc3b451c2bc9a
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2025-09-20 15:37:35 +0300

    task-5 done

--- src/routers/products.js
+++ src/routers/products.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import {
   getProductsControllers,
   getProductByIdController,
+  createProductController,
 } from '../controllers/products.js';
 
 const router = Router();
 
 router.get('/products', getProductsControllers);
 router.get('/products/:productId', getProductByIdController);
+router.post('/products', createProductController);
 
 export default router;
--- src/services/products.js
+++ src/services/products.js
@@ -7,3 +7,7 @@ export const getProducts = () => {
 export const getProductById = (productId) => {
   return ProductModel.findById(productId);
 };
+
+export const createProduct = (payload) => {
+  return ProductModel.create(payload);
+};
