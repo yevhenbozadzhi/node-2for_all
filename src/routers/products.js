63b5da6d29b5e83379c2a48781c48ec72c26025a
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2025-10-04 14:30:42 +0300

    added body validation

--- src/routers/products.js
+++ src/routers/products.js
@@ -6,13 +6,24 @@ import {
   deleteProductController,
   patchProductController,
 } from '../controllers/products.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createProductSchema,
+  updateProductSchema,
+} from '../validation/productSchema.js';
+import { validateId } from '../middlewares/validateId.js';
 
 const router = Router();
 
 router.get('/', getProductsControllers);
-router.get('/:productId', getProductByIdController);
-router.post('/', createProductController);
-router.patch('/:productId', patchProductController);
-router.delete('/:productId', deleteProductController);
+router.get('/:productId', validateId, getProductByIdController);
+router.post('/', validateBody(createProductSchema), createProductController);
+router.patch(
+  '/:productId',
+  validateId,
+  validateBody(updateProductSchema),
+  patchProductController,
+);
+router.delete('/:productId', validateId, deleteProductController);
 
 export default router;
